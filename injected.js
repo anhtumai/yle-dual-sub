34938ff4ffff93c7111e5b5e5f1d20889b781191
Anh Tu Mai <anhtumai@2001-14ba-6a28-7600--185.rev.dnainternet.fi>
2025-09-20 22:22:30 +0300

    add VTT parser

--- injected.js
+++ injected.js
@@ -1,80 +1,62 @@
 const decoder = new TextDecoder('utf-8');
 
+function parseVttFile(vttFileContent) {
+    /**
+     * Parses a VTT file content and returns an array of subtitle content.
+     * Each subtitle content is an object with 'index', 'time', and 'text' properties.
+     * 
+     * @param {string} vttText - The VTT file content.
+     */
+    const blocks = vttFileContent.split(/\n(?:\s*\n)+/);
+
+    const subtitleContentCues = blocks.filter(block => {
+        const lines = block.trim().split('\n');
+        return lines.length >= 3 &&
+            /^\d+$/.test(lines[0]) &&
+            /\d{2}:\d{2}:\d{2}\.\d{3} --> \d{2}:\d{2}:\d{2}\.\d{3}/.test(lines[1])
+    })
+
+    return subtitleContentCues.map(cue => {
+        const lines = cue.trim().split('\n');
+        return {
+            index: parseInt(lines[0], 10),
+            time: lines[1],
+            text: lines.slice(2).join('\n')
+        };
+    })
+
+}
+
 (function (xhr) {
 
-    var XHR = XMLHttpRequest.prototype;
+    const XHR = XMLHttpRequest.prototype;
 
-    var open = XHR.open;
-    var send = XHR.send;
-    //var setRequestHeader = XHR.setRequestHeader;
+    const open = XHR.open;
+    const send = XHR.send;
 
     XHR.open = function (method, url) {
         this._method = method;
         this._url = url;
-        //this._requestHeaders = {};
-        //this._startTime = (new Date()).toISOString();
 
         return open.apply(this, arguments);
     };
 
-    // XHR.setRequestHeader = function (header, value) {
-    //     this._requestHeaders[header] = value;
-    //     return setRequestHeader.apply(this, arguments);
-    // };
-
     XHR.send = function (postData) {
 
         this.addEventListener('load', function () {
-            var endTime = (new Date()).toISOString();
 
-            var myUrl = this._url ? this._url.toLowerCase() : this._url;
+            const requestedUrl = this._url ? this._url.toLowerCase() : this._url;
 
-            if (myUrl.endsWith('.vtt')) {
-                const text = decoder.decode(this.response);
-                console.log("Debug everything:", text);
+            if (requestedUrl.endsWith('.vtt')) {
+                const fullVttFileResponseText = decoder.decode(this.response);
+
+                const subtitleContents = parseVttFile(fullVttFileResponseText);
+
+                
+                console.log("Cues:", subtitleContents);
+
+
             }
-            // if(myUrl) {
-            //     console.log("Debug purpose:", postData, myUrl, this.responseHeaders, this.responseType)
-
-            //     if (postData) {
-            //         if (typeof postData === 'string') {
-            //             try {
-            //                 // here you get the REQUEST HEADERS, in JSON format, so you can also use JSON.parse
-            //                 this._requestHeaders = postData;    
-            //             } catch(err) {
-            //                 console.log('Request Header JSON decode failed, transfer_encoding field could be base64');
-            //                 console.log(err);
-            //             }
-            //         } else if (typeof postData === 'object' || typeof postData === 'array' || typeof postData === 'number' || typeof postData === 'boolean') {
-            //                 // do something if you need
-            //         }
-            //     }
-
-            //     // here you get the RESPONSE HEADERS
-            //     var responseHeaders = this.getAllResponseHeaders();
-            //     console.log("Debug part 2:", this.responseType, this.responseHeaders)
-
-            //     if ( this.responseType != 'blob' && this.responseText) {
-            //         // responseText is string or null
-            //         try {
-
-            //             // here you get RESPONSE TEXT (BODY), in JSON format, so you can use JSON.parse
-            //             var arr = this.responseText;
-
-            //             // printing url, request headers, response headers, response body, to console
-
-            //             console.log(this._url);
-            //             console.log(JSON.parse(this._requestHeaders));
-            //             console.log(responseHeaders);
-            //             console.log(JSON.parse(arr));                        
-
-            //         } catch(err) {
-            //             console.log("Error in responseType try catch");
-            //             console.log(err);
-            //         }
-            //     }
-
-            // }
         });
 
         return send.apply(this, arguments);
