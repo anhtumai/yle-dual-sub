e593bb4f008e461c5f65c0fce6581499c995dcc4
Anh Tu Mai <anhtumai@2001-14ba-6a28-7600--185.rev.dnainternet.fi>
2025-09-21 22:11:46 +0300

    format

--- contentscript.js
+++ contentscript.js
@@ -3,64 +3,62 @@ console.log("Content script loaded.");
 let addedDisplayedSubtitlesWrapper = false;
 
 function copySubtitlesWrapper(className) {
-    const displayedSubtitlesWrapper = document.createElement("div");
-    displayedSubtitlesWrapper.setAttribute("aria-live", "polite");
-    displayedSubtitlesWrapper.setAttribute("class", className);
-    displayedSubtitlesWrapper.setAttribute("id", "displayed-subtitles-wrapper");
-    return displayedSubtitlesWrapper;
+  const displayedSubtitlesWrapper = document.createElement("div");
+  displayedSubtitlesWrapper.setAttribute("aria-live", "polite");
+  displayedSubtitlesWrapper.setAttribute("class", className);
+  displayedSubtitlesWrapper.setAttribute("id", "displayed-subtitles-wrapper");
+  return displayedSubtitlesWrapper;
 }
 
-
 const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-        if (mutation.type === "childList") {
-            try {
-                if (mutation?.target?.dataset["testid"] === "subtitles-wrapper") {
-                    console.log("Subtitle wrapper changed", mutation, mutation.target.innerText);
+  mutations.forEach((mutation) => {
+    if (mutation.type === "childList") {
+      try {
+        if (mutation?.target?.dataset["testid"] === "subtitles-wrapper") {
+          console.log("Subtitle wrapper changed", mutation, mutation.target.innerText);
 
-                    const targetElement = mutation.target;
-                    targetElement.style.display = "none";
+          const targetElement = mutation.target;
+          targetElement.style.display = "none";
 
-                    if (!addedDisplayedSubtitlesWrapper) {
-                        const displayedSubtitlesWrapperElement = copySubtitlesWrapper(mutation.target.className);
-                        targetElement.parentNode.insertBefore(
-                            displayedSubtitlesWrapperElement,
-                            targetElement.nextSibling
-                        );
-                        addedDisplayedSubtitlesWrapper = true;
-                    }
-                    const displayedSubtitlesWrapper = document.getElementById("displayed-subtitles-wrapper");
-                    displayedSubtitlesWrapper.innerHTML = "";
+          if (!addedDisplayedSubtitlesWrapper) {
+            const displayedSubtitlesWrapperElement = copySubtitlesWrapper(
+              mutation.target.className,
+            );
+            targetElement.parentNode.insertBefore(
+              displayedSubtitlesWrapperElement,
+              targetElement.nextSibling,
+            );
+            addedDisplayedSubtitlesWrapper = true;
+          }
+          const displayedSubtitlesWrapper = document.getElementById("displayed-subtitles-wrapper");
+          displayedSubtitlesWrapper.innerHTML = "";
 
-                    if (mutation.addedNodes.length > 0) {
-                        const finnishText = mutation.target.innerText;
-                        const finnishSpan = document.createElement("span");
-                        finnishSpan.textContent = finnishText;
+          if (mutation.addedNodes.length > 0) {
+            const finnishText = mutation.target.innerText;
+            const finnishSpan = document.createElement("span");
+            finnishSpan.textContent = finnishText;
 
-                        const translatedEnglishSpan = document.createElement("span");
-                        translatedEnglishSpan.textContent = `Translated: ${finnishText}`; // Default to finnish text
-                        
-                        displayedSubtitlesWrapper.appendChild(finnishSpan);
-                        displayedSubtitlesWrapper.appendChild(translatedEnglishSpan)
-                    }
+            const translatedEnglishSpan = document.createElement("span");
+            translatedEnglishSpan.textContent = `Translated: ${finnishText}`; // Default to finnish text
 
-
-
-                }
-            } catch (e) {
-                console.error("Error processing mutation", e);
-            }
+            displayedSubtitlesWrapper.appendChild(finnishSpan);
+            displayedSubtitlesWrapper.appendChild(translatedEnglishSpan);
+          }
         }
-    });
+      } catch (e) {
+        console.error("Error processing mutation", e);
+      }
+    }
+  });
 });
 
 // Start observing the document for added nodes
 if (document.body instanceof Node) {
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-        characterData: true,
-    });
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    characterData: true,
+  });
 } else {
-    console.log("What is document body", document.body);
+  console.log("What is document body", document.body);
 }
--- injected.js
+++ injected.js
@@ -1,80 +1,75 @@
-const decoder = new TextDecoder('utf-8');
+const decoder = new TextDecoder("utf-8");
 
 const translationMap = new Map();
 
 async function getDeeplTranslation(text) {
-    // TODO: Implement this DeepL api call after getting the API Key
-    return `{Translated: ${text}}`
+  // TODO: Implement this DeepL api call after getting the API Key
+  return `{Translated: ${text}}`;
 }
 
 function parseVttFile(vttFileContent) {
-    /**
-     * Parses a VTT file content and returns an array of subtitle content.
-     * Each subtitle content is an object with 'index', 'time', and 'text' properties.
-     * 
-     * @param {string} vttText - The VTT file content.
-     */
-    const blocks = vttFileContent.split(/\n(?:\s*\n)+/);
+  /**
+   * Parses a VTT file content and returns an array of subtitle content.
+   * Each subtitle content is an object with 'index', 'time', and 'text' properties.
+   *
+   * @param {string} vttText - The VTT file content.
+   */
+  const blocks = vttFileContent.split(/\n(?:\s*\n)+/);
 
-    const subtitleContentCues = blocks.filter(block => {
-        const lines = block.trim().split('\n');
-        return lines.length >= 3 &&
-            /^\d+$/.test(lines[0]) &&
-            /\d{2}:\d{2}:\d{2}\.\d{3} --> \d{2}:\d{2}:\d{2}\.\d{3}/.test(lines[1])
-    })
+  const subtitleContentCues = blocks.filter((block) => {
+    const lines = block.trim().split("\n");
+    return (
+      lines.length >= 3 &&
+      /^\d+$/.test(lines[0]) &&
+      /\d{2}:\d{2}:\d{2}\.\d{3} --> \d{2}:\d{2}:\d{2}\.\d{3}/.test(lines[1])
+    );
+  });
 
-    return subtitleContentCues.map(cue => {
-        const lines = cue.trim().split('\n');
-        return {
-            index: parseInt(lines[0], 10),
-            time: lines[1],
-            text: lines.slice(2).join('\n')
-        };
-    })
-
+  return subtitleContentCues.map((cue) => {
+    const lines = cue.trim().split("\n");
+    return {
+      index: parseInt(lines[0], 10),
+      time: lines[1],
+      text: lines.slice(2).join("\n"),
+    };
+  });
 }
 
 (function (xhr) {
+  const XHR = XMLHttpRequest.prototype;
 
-    const XHR = XMLHttpRequest.prototype;
+  const open = XHR.open;
+  const send = XHR.send;
 
-    const open = XHR.open;
-    const send = XHR.send;
+  XHR.open = function (method, url) {
+    this._method = method;
+    this._url = url;
 
-    XHR.open = function (method, url) {
-        this._method = method;
-        this._url = url;
+    return open.apply(this, arguments);
+  };
 
-        return open.apply(this, arguments);
-    };
+  XHR.send = function (postData) {
+    this.addEventListener("load", function () {
+      const requestedUrl = this._url ? this._url.toLowerCase() : this._url;
 
-    XHR.send = function (postData) {
+      if (requestedUrl.endsWith(".vtt")) {
+        const fullVttFileResponseText = decoder.decode(this.response);
 
-        this.addEventListener('load', function () {
+        const subtitleContents = parseVttFile(fullVttFileResponseText);
 
-            const requestedUrl = this._url ? this._url.toLowerCase() : this._url;
+        console.log("Subtitle Contents:", subtitleContents);
 
-            if (requestedUrl.endsWith('.vtt')) {
-                const fullVttFileResponseText = decoder.decode(this.response);
+        subtitleContents.forEach(async (subtitleContent) => {
+          const { index, time, text } = subtitleContent;
+          const translatedText = await getDeeplTranslation(text);
 
-                const subtitleContents = parseVttFile(fullVttFileResponseText);
+          console.log("Get Text: ", text);
 
-                console.log("Subtitle Contents:", subtitleContents);
-
-                subtitleContents.forEach(async (subtitleContent) => {
-                    const { index, time, text } = subtitleContent;
-                    const translatedText = await getDeeplTranslation(text); 
-
-                    console.log("Get Text: ", text);
-
-                    translationMap.set(text.trim(), translatedText);
-                })
-
-
-            }
+          translationMap.set(text.trim(), translatedText);
         });
+      }
+    });
 
-        return send.apply(this, arguments);
-    };
-
+    return send.apply(this, arguments);
+  };
 })(XMLHttpRequest);
