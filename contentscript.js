0d874d3f73d2b5635b1ea38f3da9ab6f3fb0726e
Anh Tu Mai <anhtumai@2001-14ba-6a28-7600--185.rev.dnainternet.fi>
2025-09-21 22:07:44 +0300

    next step

--- contentscript.js
+++ contentscript.js
@@ -1,14 +1,51 @@
 console.log("Content script loaded.");
 
+let addedDisplayedSubtitlesWrapper = false;
+
+function copySubtitlesWrapper(className) {
+    const displayedSubtitlesWrapper = document.createElement("div");
+    displayedSubtitlesWrapper.setAttribute("aria-live", "polite");
+    displayedSubtitlesWrapper.setAttribute("class", className);
+    displayedSubtitlesWrapper.setAttribute("id", "displayed-subtitles-wrapper");
+    return displayedSubtitlesWrapper;
+}
+
+
 const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         if (mutation.type === "childList") {
             try {
                 if (mutation?.target?.dataset["testid"] === "subtitles-wrapper") {
-                    console.log("Subtitle wrapper changed", mutation);
+                    console.log("Subtitle wrapper changed", mutation, mutation.target.innerText);
 
-                    targetElement = mutation.target;
-                    //targetElement.style.display = "none";
+                    const targetElement = mutation.target;
+                    targetElement.style.display = "none";
+
+                    if (!addedDisplayedSubtitlesWrapper) {
+                        const displayedSubtitlesWrapperElement = copySubtitlesWrapper(mutation.target.className);
+                        targetElement.parentNode.insertBefore(
+                            displayedSubtitlesWrapperElement,
+                            targetElement.nextSibling
+                        );
+                        addedDisplayedSubtitlesWrapper = true;
+                    }
+                    const displayedSubtitlesWrapper = document.getElementById("displayed-subtitles-wrapper");
+                    displayedSubtitlesWrapper.innerHTML = "";
+
+                    if (mutation.addedNodes.length > 0) {
+                        const finnishText = mutation.target.innerText;
+                        const finnishSpan = document.createElement("span");
+                        finnishSpan.textContent = finnishText;
+
+                        const translatedEnglishSpan = document.createElement("span");
+                        translatedEnglishSpan.textContent = `Translated: ${finnishText}`; // Default to finnish text
+                        
+                        displayedSubtitlesWrapper.appendChild(finnishSpan);
+                        displayedSubtitlesWrapper.appendChild(translatedEnglishSpan)
+                    }
+
+
+
                 }
             } catch (e) {
                 console.error("Error processing mutation", e);
