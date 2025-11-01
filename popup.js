b9dc647edfb43ba1d34b06ff03b2c678d60fe5ba
anhtumai <tu.mai@outlook.com>
2025-11-01 17:23:30 +0200

    ...

--- popup.js
+++ popup.js
@@ -1,17 +1,4 @@
-const accordions = document.querySelectorAll(".accordion");
-
-accordions.forEach((accordion) => {
-    const header = accordion.querySelector(".accordion-header");
-
-    header.addEventListener("click", () => {
-        accordion.classList.toggle("active");
-    });
-});
-
-const toggles = document.querySelectorAll(".toggle");
-
-toggles.forEach((toggle) => {
-    toggle.addEventListener("click", () => {
-        toggle.classList.toggle("active");
-    });
+document.getElementById("setupLink").addEventListener("click", function (e) {
+  e.preventDefault();
+  chrome.runtime.openOptionsPage();
 });
