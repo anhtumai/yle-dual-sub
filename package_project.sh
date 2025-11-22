5ae220d3034b1c1e574c05d6cf2fd24a6041f8af
anhtumai <tu.mai@outlook.com>
2025-11-22 13:40:57 +0200

    add a tool to zip neccessary file for publishing

---
+++ package_project.sh
@@ -0,0 +1,11 @@
+zip -r yle-dual-sub-extension.zip \
+  manifest.json \
+  background.js \
+  database.js \
+  utils.js \
+  contentscript.js \
+  inject.js \
+  injected.js \
+  styles.css \
+  popup.html \
+  extension-options-page/dist/
